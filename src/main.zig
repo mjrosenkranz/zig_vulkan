bd4db4f18161557a1c32a21ae17fa6ca71f9df2d
xenone <rosenkranz1999@gmail.com>
2021-04-09 22:27:00 -0400

    attempting to refactor window class to be more general

--- src/main.zig
+++ src/main.zig
@@ -5,35 +5,41 @@ const os = std.os;
 const stdin = std.io.getStdIn().reader();
 const Allocator = std.mem.Allocator;
 
+const allocator = std.heap.c_allocator;
+var window = undefined;
+var instance = undefined;
 var quit = false;
 
-fn handler(sig: i32, info: *const os.siginfo_t, ctx_ptr: ?*const c_void) callconv(.C) void {
-    if (sig == os.SIGINT)
-        quit = true;
-
-    std.debug.warn("\nShutting down...\n", .{});
-}
-
-pub fn main() !void {
-    const window = try win.glfwwin(800, 600);
-    defer window.destroy();
-
+pub fn init() !void {
     var act = os.Sigaction{
         .handler = .{ .sigaction = handler },
         .mask = os.empty_sigset,
         .flags = (os.SA_SIGINFO | os.SA_RESETHAND),
     };
     os.sigaction(os.SIGINT, &act, null);
+    window = try win.glfwwin(800, 600);
+    instance = try vk.createInstance(allocator, @TypeOf(window), window);
+}
 
-    const allocator = std.heap.c_allocator;
+pub fn shutdown() void {
+    vk.destroyInstance(instance);
+    window.destroy();
+}
 
-    var instance = try vk.createInstance(allocator, @TypeOf(window), window);
-    defer vk.destroyInstance(instance);
-
+pub fn main() !void {
+    try init();
     while (!quit) {
         var e: win.Event = window.poll();
         if (e == win.Event.Close) {
             quit = true;
         }
     }
+    shutdown();
+}
+
+fn handler(sig: i32, info: *const os.siginfo_t, ctx_ptr: ?*const c_void) callconv(.C) void {
+    if (sig == os.SIGINT)
+        quit = true;
+    std.debug.warn("\nShutting down...\n", .{});
+    shutdown();
 }
